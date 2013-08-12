777332b38191128ca0f7bdbd91df640fa78277db
MrDHat <ktyaks@gmail.com>
2013-08-13 01:00:50 +0530

    Added parallax Effect

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -6,7 +6,6 @@
  * Date: 10-Sep-2012
  * Version: 1.0.1
  */
-if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
 
 /**
  * jQuery.ScrollTo - Easy element scrolling using jQuery.
